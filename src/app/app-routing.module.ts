3b8fc8b57886ebdb9833f8b7c52fd0dc503245b9
Sebastian Amaya <sjamaya@uninorte.edu.co>
2020-12-10 10:29:09 -0500

    angular vistas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import {LandingComponent} from './components/landing/landing.component';
 import {LoginComponent} from './components/login/login.component';
 import {RegisterComponent} from './components/register/register.component';
-import { ChangepComponent } from './components/changep/changep.component';
+import {ChangepComponent} from './components/changep/changep.component';
+import {CreateCommentComponent} from './components/create-comment/create-comment.component';
+import {Changep2Component} from './components/changep2/changep2.component';
+import {CommentsComponent} from './components/comments/comments.component';
+import {CreateEntryComponent} from './components/create-entry/create-entry.component';
 const routes: Routes = [
   {
     path: '',
@@ -21,6 +25,22 @@ const routes: Routes = [
     path: 'user/change/password',
     component: ChangepComponent,
     data: { title: 'Cambiar contraseña' }
+  },{
+    path: 'user/change/password2',
+    component: Changep2Component,
+    data: { title: 'Cambiar contraseña' }
+  },{
+    path: 'user/view/comments',
+    component: CommentsComponent,
+    data: { title: 'Ver comentarios' }
+  },{
+    path: 'user/create/comments',
+    component: CreateCommentComponent,
+    data: { title: 'Crear comentarios' }
+  },{
+    path: 'user/create/entry',
+    component: CreateEntryComponent,
+    data: { title: 'Crear entrada' }
   },{
     path: '**',
     redirectTo: '',
